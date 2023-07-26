3bcb25a3ac614b728edd0262b487414f66ebea2e
lepsic <Lepsick2003@yandex.ru>
2023-07-26 19:15:42 +0300

    отчаяние

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [config('HOST')]
+ALLOWED_HOSTS = ['185.233.186.146']
 
 # Application definition
 
