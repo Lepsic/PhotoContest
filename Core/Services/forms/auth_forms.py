a97c61e63ca949b77a1d0cd74b03ea211c55a445
Lepsic <Lepsick2003@yandex.ru>
2023-10-15 00:57:36 +0300

    Переделал (social) не работает

--- Core/Services/forms/auth_forms.py
+++ Core/Services/forms/auth_forms.py
@@ -1,19 +1,19 @@
-from django import forms
-
-from Services.service.auth.service_validate import ServiceValidateUserData
-
-
-class UserCreationsForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.service = ServiceValidateUserData(form=self)
-
-    username = forms.CharField(max_length=40, label='Имя пользователя')
-    name = forms.CharField(max_length=100, label='Имя')
-    pas1 = forms.CharField(max_length=40, widget=forms.PasswordInput)
-    pas2 = forms.CharField(max_length=40, widget=forms.PasswordInput)
-
-
-    def clean(self):
-        self.service.run_validate()
-
+# from django import forms
+#
+# from Services.service.auth.service_validate import ServiceValidateUserData
+#
+#
+# class UserCreationsForm(forms.Form):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.service = ServiceValidateUserData(form=self)
+#
+#     username = forms.CharField(max_length=40, label='Имя пользователя')
+#     name = forms.CharField(max_length=100, label='Имя')
+#     pas1 = forms.CharField(max_length=40, widget=forms.PasswordInput)
+#     pas2 = forms.CharField(max_length=40, widget=forms.PasswordInput)
+#
+#
+#     def clean(self):
+#         self.service.run_validate()
+#
--- Core/Services/views/auth.py
+++ Core/Services/views/auth.py
@@ -5,25 +5,21 @@ from allauth.account.views import SignupView
 
 from ..forms import auth_forms
 from ..forms import SocialForm
-from Services.service.auth.service_creation_user import SaveUser
+from Services.service.auth.service_registration import RegistrationUserService
+
 
 def create_user(request):
     if request.method == 'POST':
-        form = auth_forms.UserCreationsForm(request.POST)
-        if form.is_valid():
-            save_user_class = SaveUser(form)
-            save_user_class.save_user()
+        service = RegistrationUserService(request.POST)
+        if service.is_valid():
+            service.process()
             return redirect('login')
         else:
-            return render(request, 'auth/create.html', {'form': form})
+            return render(request, 'auth/create.html', {'form': service})
     else:
-        form = auth_forms.UserCreationsForm
-        return render(request, 'auth/create.html', {'form': form})
+        service = RegistrationUserService
+        return render(request, 'auth/create.html', {'form': service})
 
 
 class LogoutView(Logout):
     next_page = reverse_lazy('login')
-
-
-
-
